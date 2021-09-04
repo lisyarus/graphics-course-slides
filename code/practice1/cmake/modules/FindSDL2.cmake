1cc85bb50d3dd9540952db0c854377b8e3c50f12
lisyarus <lisyarus@gmail.com>
2021-09-04 15:18:08 +0300

    Fixes

--- code/practice1/cmake/modules/FindSDL2.cmake
+++ code/practice1/cmake/modules/FindSDL2.cmake
@@ -1,6 +1,12 @@
 if(NOT SDL2_FOUND)
+	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+		set(_SDL2_LIB_SUFFIX x64)
+	else()
+		set(_SDL2_LIB_SUFFIX x86)
+	endif()
+
 	find_path(SDL2_INCLUDE_DIRS NAMES "SDL2/SDL.h" PATHS "${SDL2_ROOT}/include")
-	find_library(SDL2_LIBRARIES NAMES "SDL2" PATHS "${SDL2_ROOT}/lib")
+	find_library(SDL2_LIBRARIES NAMES "SDL2" PATHS "${SDL2_ROOT}/lib/${_SDL2_LIB_SUFFIX}")
 
 	include(FindPackageHandleStandardArgs)
 	find_package_handle_standard_args(SDL2 DEFAULT_MSG SDL2_INCLUDE_DIRS SDL2_LIBRARIES)
--- code/practice1/main.cpp
+++ code/practice1/main.cpp
@@ -1,4 +1,10 @@
+#ifdef WIN32
+#include <SDL.h>
+#undef main
+#else
 #include <SDL2/SDL.h>
+#endif
+
 #include <GL/glew.h>
 
 #include <string_view>
